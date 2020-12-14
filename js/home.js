4b25d4fe49400b9b837f8a64210ecd935e266a23
tranquangnhan <tranquangnhan1606@gmail.com>
2020-12-14 17:19:04 +0700

    trang chu

---
+++ js/home.js
@@ -0,0 +1,39 @@
+window.onload = function (){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:15,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+   
+    document.getElementById("defaultOpen").click();
+   
+}
+
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
