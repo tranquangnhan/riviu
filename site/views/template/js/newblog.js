665c5d145ea300743bc2af807d5551da0c619e38
tranquangnhan <tranquangnhan1606@gmail.com>
2020-12-19 12:16:05 +0700

    add template baiviet

---
+++ site/views/template/js/newblog.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    $(function() {
+
+        $("#rateYo").rateYo({
+            rating: 2,
+            fullStar: true,
+            starWidth: "25px",
+            startImg: "../images/star_fill (1).png", //RED
+        });
+    });
+}
